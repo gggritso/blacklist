f6e1cb8a9d684bd87a134bf2927dea4595eac9fa
George Gritsouk <george.gritsouk@gmail.com>
2019-08-10 12:56:26 -0400

    Linting

--- src/js/index.js
+++ src/js/index.js
@@ -6,18 +6,18 @@ const CARDS_PER_ROUND = 4,
 
 $(() => {
   // Is this already a game? Read hash and check!
-  var cards = decodeGameURL(window.location.hash.slice(1));
+  const cards = decodeGameURL(window.location.hash.slice(1));
   if (!cards) {
     generateNewGame();
   } else {
-    for (var i = 0; i < cards.length; i += 1) {
+    for (let i = 0; i < cards.length; i += 1) {
       appendCard(i, cards[i]);
     }
   }
 
   // Event Listeners
   $("body").on("click", ".reveal-card", event => {
-    var cardID = $(event.target).data("card-id");
+    const cardID = $(event.target).data("card-id");
     $(".card[data-card-id=" + cardID + "]").show();
     $(event.target).hide();
   });
@@ -28,16 +28,16 @@ $(() => {
 });
 
 const encodeGameURL = cards => {
-  var rets = [],
+  const rets = [],
     enc = [];
 
-  var currentCard;
-  for (var y = 0; y < cards.length; y += 1) {
+  let currentCard;
+  for (let y = 0; y < cards.length; y += 1) {
     enc = [];
     currentCard = cards[y];
 
-    for (var i = 0; i < currentCard[1].length; i += 1) {
-      enc.push(CATEGORIES.indexOf(currentCard[1][i]).toString(16));
+    for (let i = 0; i < currentCard[1].length; i += 1) {
+      enc.push(CATEGORIES.indexOf(currentCard[1][i]).toString(16)); //eslint-disable-line no-magic-numbers
     }
 
     rets.push(currentCard[0] + enc.join("+"));
@@ -47,67 +47,60 @@ const encodeGameURL = cards => {
 };
 
 const decodeGameURL = url => {
-  var cards = [],
-    letter = "";
+  const cards = [];
 
-  if (url == "#" || url == "") {
+  if (url === "#" || url === "") {
     return null;
   }
 
-  var hashes = url.split("|");
+  const hashes = url.split("|");
 
-  for (var y = 0; y < hashes.length; y += 1) {
-    var letter = hashes[y].slice(0, 1),
+  for (let y = 0; y < hashes.length; y += 1) {
+    const letter = hashes[y].slice(0, 1),
       categoryNumbers = hashes[y].slice(1).split("+"),
       categories = [];
 
-    for (var i = 0; i < categoryNumbers.length; i += 1) {
-      var categoryIndex = parseInt(categoryNumbers[i], 16);
+    for (let i = 0; i < categoryNumbers.length; i += 1) {
+      const categoryIndex = parseInt(categoryNumbers[i], 16);
       categories.push(CATEGORIES[categoryIndex]);
     }
 
-    var card = [letter, categories];
-    cards.push(card);
+    cards.push([letter, categories]);
   }
 
   return cards;
 };
 
 const appendCard = (cardIndex, card) => {
-  var s = "";
-  s +=
-    '<div class="card" data-card-id="' +
-    cardIndex +
-    '" style="display: none;">';
+  let s = "";
+  s += '<div class="card" data-card-id="' + cardIndex + '" style="display: none;">';
   s += '<span class="letter">' + card[0] + "</span>";
   s += "<ol>";
 
-  for (var i = 0; i < card[1].length; i += 1) {
+  for (let i = 0; i < card[1].length; i += 1) {
     s += "<li>" + card[1][i] + "</li>";
   }
 
   s += "</ol></div>";
-  s +=
-    '<button data-card-id="' +
-    cardIndex +
-    '" class="reveal-card">Reveal Card</button><br>';
+  s += '<button data-card-id="' + cardIndex + '" class="reveal-card">Reveal Card</button><br>';
 
   $("#game-container").append(s);
 };
 
 const generateNewCard = () => {
-  var card = [],
+  const card = [],
     r = Math.floor(Math.random() * LETTERS.length),
-    letter = LETTERS[r],
-    category = "";
+    letter = LETTERS[r];
+
+  let category = "";
 
   card[0] = letter;
   card[1] = [];
 
-  for (var i = 0; i < 12; i += 1) {
+  for (let i = 0; i < 12; i += 1) {
     r = Math.floor(Math.random() * CATEGORIES.length);
     category = CATEGORIES[r];
-    while (PICKED.indexOf(category) != -1) {
+    while (PICKED.indexOf(category) !== -1) {
       r = Math.floor(Math.random() * CATEGORIES.length);
       category = CATEGORIES[r];
     }
@@ -122,15 +115,14 @@ const generateNewGame = () => {
   $("#game-container").html("");
   window.location.hash = "";
 
-  let card;
-  var cards = [];
+  const cards = [];
 
-  for (var i = 0; i < CARDS_PER_ROUND; i += 1) {
-    card = generateNewCard();
+  for (let i = 0; i < CARDS_PER_ROUND; i += 1) {
+    const card = generateNewCard();
     cards.push(card);
     appendCard(i, card);
   }
 
-  var gameURL = window.location.origin + "/#" + encodeGameURL(cards);
+  const gameURL = window.location.origin + "/#" + encodeGameURL(cards);
   $("#game-url").val(gameURL);
 };
