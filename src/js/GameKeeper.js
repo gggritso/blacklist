be22cbcebd862bd8c912ecdf95b67402dac6a6a8
George Gritsouk <989898+gggritso@users.noreply.github.com>
2019-11-09 16:39:34 -0500

    Encapsulate game logic in separate class

---
+++ src/js/GameKeeper.js
@@ -0,0 +1,109 @@
+import { CATEGORIES } from "./categories";
+import { LETTERS } from "./letters";
+
+const PICKED_LETTERS = [];
+const PICKED_CATEGORIES = [];
+
+const CARDS_PER_GAME = 4;
+const LETTERS_PER_CARD = 12;
+
+export class GameKeeper {
+  constructor(encodedCards) {
+    if (encodedCards) {
+      this.cards = this.decodeGameURL(encodedCards);
+    } else {
+      this.cards = this.generateCards(CARDS_PER_GAME);
+    }
+
+    this.URL = window.location.origin + "/#" + this.encodeGameURL(this.cards);
+  }
+
+  generateCards(count) {
+    const cards = [];
+
+    for (let i = 0; i < count; i += 1) {
+      cards.push(this.generateCard());
+    }
+
+    return cards;
+  }
+
+  generateCard() {
+    const card = {
+      letter: null,
+      categories: [],
+      isVisible: false
+    };
+
+    let r = Math.floor(Math.random() * LETTERS.length);
+    let letter = LETTERS[r];
+
+    while (PICKED_LETTERS.indexOf(letter) !== -1) {
+      r = Math.floor(Math.random() * LETTERS.length);
+      letter = LETTERS[r];
+    }
+
+    PICKED_LETTERS.push(letter);
+
+    let category = "";
+
+    card.letter = letter;
+
+    for (let i = 0; i < LETTERS_PER_CARD; i += 1) {
+      r = Math.floor(Math.random() * CATEGORIES.length);
+      category = CATEGORIES[r];
+      while (PICKED_CATEGORIES.indexOf(category) !== -1) {
+        r = Math.floor(Math.random() * CATEGORIES.length);
+        category = CATEGORIES[r];
+      }
+      card.categories.push(category);
+      PICKED_CATEGORIES.push(category);
+    }
+
+    return card;
+  }
+
+  encodeGameURL(cards) {
+    let rets = [],
+      enc = [];
+
+    let currentCard;
+    for (let y = 0; y < cards.length; y += 1) {
+      enc = [];
+      currentCard = cards[y];
+
+      for (let i = 0; i < currentCard.categories.length; i += 1) {
+        enc.push(CATEGORIES.indexOf(currentCard.categories[i]).toString(16)); //eslint-disable-line no-magic-numbers
+      }
+
+      rets.push(currentCard.letter + enc.join("+"));
+    }
+
+    return rets.join("|");
+  }
+
+  decodeGameURL(url) {
+    const cards = [];
+
+    if (url === "#" || url === "") {
+      return null;
+    }
+
+    const hashes = url.split("|");
+
+    for (let y = 0; y < hashes.length; y += 1) {
+      const letter = hashes[y].slice(0, 1),
+        categoryNumbers = hashes[y].slice(1).split("+"),
+        categories = [];
+
+      for (let i = 0; i < categoryNumbers.length; i += 1) {
+        const categoryIndex = parseInt(categoryNumbers[i], 16);
+        categories.push(CATEGORIES[categoryIndex]);
+      }
+
+      cards.push({ letter, categories, isVisible: false });
+    }
+
+    return cards;
+  }
+}
--- src/js/components/game.jsx
+++ src/js/components/game.jsx
@@ -1,29 +1,21 @@
 import React, { Component, Fragment } from "react";
 
 import { Card } from "./card.jsx";
-
-import { CATEGORIES } from "../categories";
-import { LETTERS } from "../letters";
-
-const PICKED_LETTERS = [];
-const PICKED_CATEGORIES = [];
+import { GameKeeper } from "../GameKeeper";
 
 export class Game extends Component {
   constructor(props) {
     super(props);
 
-    this.generateCards = this.generateCards.bind(this);
+    this.keeper = new GameKeeper(window.location.hash.slice(1));
+
     this.revealNextCard = this.revealNextCard.bind(this);
     this.setCurrentCard = this.setCurrentCard.bind(this);
 
-    let cards = this.decodeGameURL(window.location.hash.slice(1));
-
-    if (!cards) cards = this.generateCards(4);
-
     this.state = {
-      url: window.location.origin + "/#" + this.encodeGameURL(cards),
-      currentCard: cards[0],
-      cards
+      url: this.keeper.URL,
+      cards: this.keeper.cards,
+      currentCard: this.keeper.cards[0]
     };
   }
 
@@ -62,94 +54,5 @@ export class Game extends Component {
       ...this.state,
       currentCard: card
     });
-  }
-
-  generateCards(count) {
-    const cards = [];
-
-    for (let i = 0; i < count; i += 1) {
-      cards.push(this.generateCard());
-    }
-
-    return cards;
-  }
-
-  generateCard() {
-    const card = {
-      letter: null,
-      categories: [],
-      isVisible: false
-    };
-
-    let r = Math.floor(Math.random() * LETTERS.length);
-    let letter = LETTERS[r];
-
-    while (PICKED_LETTERS.indexOf(letter) !== -1) {
-      r = Math.floor(Math.random() * LETTERS.length);
-      letter = LETTERS[r];
-    }
-
-    PICKED_LETTERS.push(letter);
-
-    let category = "";
-
-    card.letter = letter;
-
-    for (let i = 0; i < 12; i += 1) {
-      r = Math.floor(Math.random() * CATEGORIES.length);
-      category = CATEGORIES[r];
-      while (PICKED_CATEGORIES.indexOf(category) !== -1) {
-        r = Math.floor(Math.random() * CATEGORIES.length);
-        category = CATEGORIES[r];
-      }
-      card.categories.push(category);
-      PICKED_CATEGORIES.push(category);
-    }
-
-    return card;
-  }
-
-  encodeGameURL(cards) {
-    let rets = [],
-      enc = [];
-
-    let currentCard;
-    for (let y = 0; y < cards.length; y += 1) {
-      enc = [];
-      currentCard = cards[y];
-
-      for (let i = 0; i < currentCard.categories.length; i += 1) {
-        enc.push(CATEGORIES.indexOf(currentCard.categories[i]).toString(16)); //eslint-disable-line no-magic-numbers
-      }
-
-      rets.push(currentCard.letter + enc.join("+"));
-    }
-
-    return rets.join("|");
-  }
-
-  decodeGameURL(url) {
-    const cards = [];
-
-    if (url === "#" || url === "") {
-      return null;
-    }
-
-    const hashes = url.split("|");
-
-    for (let y = 0; y < hashes.length; y += 1) {
-      const letter = hashes[y].slice(0, 1),
-        categoryNumbers = hashes[y].slice(1).split("+"),
-        categories = [];
-
-      for (let i = 0; i < categoryNumbers.length; i += 1) {
-        const categoryIndex = parseInt(categoryNumbers[i], 16);
-        categories.push(CATEGORIES[categoryIndex]);
-      }
-
-      cards.push({ letter, categories, isVisible: false });
-    }
-
-    return cards;
   }
 }
